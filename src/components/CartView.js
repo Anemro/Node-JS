f745fdcb608c0b4b63deb388bc5678943d6b8b68
Anemro <97135868+Anemro@users.noreply.github.com>
2022-06-12 22:14:04 -0300

    Add files via upload

--- src/components/CartView.js
+++ src/components/CartView.js
@@ -4,6 +4,7 @@ import useCartContex from '../store/CartContext'
 
 function CartView() {
     const { cart, cleanCart, removeFromCart,calcPriceCart} = useCartContex();
+    const [orderID, setOrderID] = React.useState(false);
     function buy () {
         const itemsToBuy = cart.map((item) => ({
             title: item.title,
@@ -24,37 +25,47 @@ function CartView() {
         cleanCart()
     }
 
-
-
+ 
     if(cart.length === 0){
-        return (
-            <>
-            <h4>No hay item en el carrito</h4>
-            <a href="/">Volver al catalogo</a>
-            </>
-            
+        if (orderID){
+            return (
+                <>
+                <h4>Gracias por tu compra</h4>
+                <h5>{orderID}</h5>
+                <Link to="/">Volver al catalogo</Link>
+                </>
             )
-        }else {
-            return <div>
-            { cart.map(  itemCart => {
-                return (
-                    <>
-                    <div key={itemCart.id}>
-                    <h2>{ itemCart.title }</h2>
-                    <h2>{ itemCart.cant }</h2>
-                    <h2>${ (itemCart.cant * itemCart.price).toFixed(2) }</h2>
-                    </div>
-                    <button onClick={() => removeFromCart(itemCart.id)}>x</button>
-                    </>
-                    )
-                })}
-         
-               
-                <button className="btn btn-primary" onClick={cleanCart}>Vaciar Carrito</button>
-                <button className="btn btn-primary" onClick={buy}>Comprar</button>
-                </div>
+        }else{
+            return (
+                <>
+                <h4>No hay item en el carrito</h4>
+                <Link to="/">Volver al catalogo</Link>
+                </>
+                
+                )
             }
-        }
+            }else {
+                return <div>
+                { cart.map(  itemCart => {
+                    return (
+                        <>
+                        <div key={itemCart.id}>
+                        <h2>{ itemCart.title }</h2>
+                        <h2>{ itemCart.cant }</h2>
+                        <h2>${ (itemCart.cant * itemCart.price).toFixed(2) }</h2>
+                        </div>
+                        <button onClick={() => removeFromCart(itemCart.id)}>x</button>
+                        </>
+                        )
+                    })}
+             
+                   
+                    <button className="btn btn-primary" onClick={cleanCart}>Vaciar Carrito</button>
+                    <button className="btn btn-primary" onClick={buy}>Comprar</button>
+                    </div>
+                }
+            }
+
                 
                 
                 
